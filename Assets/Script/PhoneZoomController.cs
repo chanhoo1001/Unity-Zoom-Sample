192073e106e7b206b5d2533c90c37ea319def3c4
Chung Chanhoo <jchan1001@naver.com>
2018-09-11 23:50:43 +0900

    Add Smartphone Zoom Sample

---
+++ Assets/Script/PhoneZoomController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PhoneZoomController : MonoBehaviour {
+
+    const float ZOOM_SPEED = 0.05f;
+    float mMaxZoomOutSize;
+    float mMaxZoomInSize;
+
+    void Start () {
+        mMaxZoomOutSize = Camera.main.fieldOfView;
+        mMaxZoomInSize = 10f;
+    }
+
+    void Update () {
+        if (Input.touchCount == 2)
+        {
+            Touch firstTouch = Input.GetTouch(0);
+            Touch secondTouch = Input.GetTouch(1);
+            Vector2 firstTouchPrevPos = firstTouch.position - firstTouch.deltaPosition;
+            Vector2 secondTouchPrevPos = secondTouch.position - secondTouch.deltaPosition;
+
+            float prevTouchDeltaMag = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+            float touchDeltaMag = (firstTouch.position - secondTouch.position).magnitude;
+            float diff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView + diff * ZOOM_SPEED, mMaxZoomInSize, mMaxZoomOutSize);
+        }
+    }
+}
